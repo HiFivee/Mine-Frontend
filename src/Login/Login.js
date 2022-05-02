a20f27d77d905d083c8339191df492cc17159350
Komputer-P <wlghks0314@gmail.com>
2022-05-02 17:04:12 +0900

    postman mock server api test completed

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -17,8 +17,8 @@ function Login() {
         let item = {email, password};
         console.warn(item);
         
-        let result = await fetch("http://localhost:8080/api/login",{
-            mehtod: 'POST',
+        let result = await fetch("https://0bfabe7c-c087-4dcb-bf72-9ab5e3650b87.mock.pstmn.io/api/auth/login",{
+            method: 'POST',
             body: JSON.stringify(item),
             headers:{
                 "Content-Type": "application/json",
@@ -39,7 +39,7 @@ function Login() {
             <br/>
             <input type="password" placeholder="password" value={password} onChange={(e)=>setPassword(e.target.value)} />
             <br/>
-            <button>Login</button>
+            <button onClick = {() => login()}>Login</button>
             <br/>
             <button onClick = {() => history(-1)}>Back</button>
             </div>
