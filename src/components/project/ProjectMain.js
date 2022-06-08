cc5f113bd8286cc28e0a55b77679dab6416d89d7
jxxnixx <dkepffk10@sookmyung.ac.kr>
2022-06-08 23:36:59 +0900

    .

--- src/components/project/ProjectMain.js
+++ src/components/project/ProjectMain.js
@@ -17,7 +17,7 @@ export default function ProjectMain() {
     <div className="py-12 bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="lg:text-center">
-          <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">주요 기능</h2>
+          <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">M i n e</h2>
           <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
             프로젝트를 해볼 수 있는 최적의 환경
           </p>
